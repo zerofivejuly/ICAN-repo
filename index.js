1f7f9c8f8ba47b66dbec2953491f5d1140c9d6b6
George Ndianabasi <zerofivejuly@gmail.com>
2025-03-18 12:41:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,87 @@
-//===================================COPYRIGHT DATE======================================
-const year = document.getElementById('year');
-const thisYear = new Date().getFullYear();
-year.setAttribute('datetime', thisYear);
-year.textContent = thisYear;
+//===================================COPYRIGHT DATE====================================== 
 
-//==============================TOGGLE BTW MAIN-SIGNIN AND MAIN-SIGN-UP==================
-const acct1 = document.querySelector('#acct1');
-const acct2 = document.querySelector('#acct2');
-const mainSignIn = document.querySelector('.main-signin');
-const mainSignUp = document.querySelector('.main-signup');
-const main = document.querySelector('.main');
+const year = document.getElementById('year'); 
 
+const thisYear = new Date().getFullYear(); 
 
-acct1.addEventListener('click', function() {
-    mainSignIn.classList.add('signIn');
-    mainSignUp.classList.add('signIn');
-    main.style.margin = 'auto'
-});
+year.setAttribute('datetime', thisYear); 
 
-acct2.addEventListener('click', function() {
-    mainSignIn.classList.remove('signIn');
-    mainSignUp.classList.remove('signIn');
-    main.style.margin = '15rem'
-});
+year.textContent = thisYear; 
+
+ 
+
+ 
+
+//==============================TOGGLE BTW MAIN-SIGNIN AND MAIN-SIGN-UP======================================= 
+
+const acct1 = document.querySelector('#acct1'); 
+
+const acct2 = document.querySelector('#acct2'); 
+
+const mainSignIn = document.querySelector('.main-signin'); 
+
+const mainSignUp = document.querySelector('.main-signup'); 
+
+const create = document.querySelector('.create'); 
+
+const forgot = document.querySelector('.forgot'); 
+
+const back = document.querySelector('.back'); 
+
+const mainReset = document.querySelector('.main-reset'); 
+
+const memberPortal = document.querySelector('.member-portal'); 
+
+const main = document.querySelector('.main'); 
+
+ 
+
+acct1.addEventListener('click', function() { 
+
+    mainSignIn.classList.add('signIn'); 
+
+    mainSignUp.classList.add('signIn'); 
+
+    main.style.margin = 'auto' 
+
+}); 
+
+ 
+
+acct2.addEventListener('click', function() { 
+
+    mainSignIn.classList.remove('signIn'); 
+
+    mainSignUp.classList.remove('signIn'); 
+
+    main.style.margin = '15rem' 
+
+}); 
+
+ 
+
+ 
+
+//=======================TOGGLE BTW MAIN-SIGNIN AND FORGOT PASSWORD=========================================== 
+
+forgot.addEventListener('click', function() { 
+
+    mainSignIn.classList.add('signIn'); 
+
+    mainReset.classList.add('swap'); 
+
+    memberPortal.style.display = 'none';  
+
+}); 
+
+ 
+
+back.addEventListener('click', function() { 
+
+    mainSignIn.classList.remove('signIn'); 
+
+    mainReset.classList.remove('swap'); 
+
+    memberPortal.style.display = 'block';  
+
+}); 
